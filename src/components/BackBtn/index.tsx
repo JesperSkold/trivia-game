dbbe38fd6ecd182f2ecf33515bf8b47bf2a48fd3
Jesper Olsson <skoldfold@gmail.com>
2023-11-23 00:17:38 +0100

    Small cleanup for backbtn

--- src/components/BackBtn/index.tsx
+++ src/components/BackBtn/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import {
-  goBack,
-} from "../../features/gameSlice"
-import { AppDispatch } from '../../store/store'
-import {BiArrowBack} from 'react-icons/bi'
-import styles from './style.module.scss'
+import React from "react"
+import { useDispatch } from "react-redux"
+import { goBack } from "../../features/gameSlice"
+import { AppDispatch } from "../../store/store"
+import { BiArrowBack } from "react-icons/bi"
+import styles from "./style.module.scss"
 
 const BackBtn = () => {
   const dispatch = useDispatch<AppDispatch>()
   return (
-    <BiArrowBack className={styles.backBtn} onClick={() => dispatch(goBack())}>{'<'}</BiArrowBack>
+    <BiArrowBack
+      className={styles.backBtn}
+      onClick={() => dispatch(goBack())}
+    />
   )
 }
 
