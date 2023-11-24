6d6db24390afbd34b752e3e7a7ab2d7c3ce6ebf8
Jesper Olsson <skoldfold@gmail.com>
2023-11-24 15:11:47 +0100

    Prettify root

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import './styles/global-styles.module.scss';
-import './styles/colors.module.scss'
-import App from './app';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
+import { store } from "./store/store"
+import "./styles/global-styles.module.scss"
+import "./styles/colors.module.scss"
+import App from "./app"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>
-);
+)
